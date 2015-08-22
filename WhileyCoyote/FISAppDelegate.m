f6fea720a5cc81206ddca5c2952ee2a29874791b
azmuth94 <dannyaksenov@gmail.com>
2015-08-22 18:18:55 -0400

    meep meep!

--- WhileyCoyote/FISAppDelegate.m
+++ WhileyCoyote/FISAppDelegate.m
@@ -14,11 +14,22 @@
 @implementation FISAppDelegate
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
-    // Write your loop here!
     
+    NSInteger steps = 0;
+    NSUInteger anvil = arc4random_uniform(25) + 26;
     
-    
-    
+    do {
+        if ((steps%10 == 0) && steps > 0) {
+            NSLog(@"YOU'RE CUCKOO!");
+        }
+        
+        if (steps == anvil) {
+            NSLog(@"SMASH!");
+        }
+        
+        NSLog(@"Meep!Meep!");
+        steps++;
+    } while ( steps <= anvil );
     
     return YES;
 }
